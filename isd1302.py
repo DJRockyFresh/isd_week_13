c9a1cddd72e32eb92de884e83530c0632b304797
DJRockyFresh <43992131+DJRockyFresh@users.noreply.github.com>
2018-12-18 13:42:13 +0000

    Update isd1302.py

--- isd1302.py
+++ isd1302.py
@@ -1,19 +1,33 @@
-class Animal(object):
-    def __init__ (self, subclass):
-        self.subclass = subclass
-    def getName (self):
-        return self.name
+class Animal():
+    def __init__ (self, name, lifespan):
+        self.name = name
+        self.lifespan = lifespan
+    def details (self):
+        print("Name: ",self.name, "Lifespan: ", self.lifespan)
 
 class Mammal(Animal):
-    def __init__ (self, subclass):
+    def __init__ (self, name, lifespan, species):
+        Animal.__init__(self, name, lifespan)
+        self.species = species
+        print("Species: ", self.species)
+        
+class Insect(Animal):
+    def __init__ (self, name, lifespan, species, size):
+        Animal.__init__(self, name, lifespan)
+        self.species = species
+        self.size = size
+        print("Species: ", self.species,)
+
+anml1 = Mammal("Dog", "13 years", "Mammal")
+print(anml1.name,",", anml1.lifespan)
+
+print("---------------------------------------")
+
+inc1 = Insect("Bee", "150 days", "Incect", "10 cm")
+print(inc1.name,",", inc1.lifespan, ",", inc1.size)
         
 
-class Insect(Animal):
-    def __init__ (self, ):
 
-class Dog(Mammal):
-    def __init__ (self, ):
 
-class Bee(Insect):
-    def __init__ (self, ):
+
 
